94bf36c9a595a490b56f71fefc2f060466cdb98f
Jlit1001 <jordanlittell@yahoo.com>
2014-07-02 14:19:19 -0700

    started pricing front-end

--- pricing/index.php
+++ pricing/index.php
@@ -1,14 +1,48 @@
 <?php 	
+	
 	include("../partials/header.php");
 	?>
 	<section>
 		<h1>Pricing Inquiry</h1>
 	</section>
+	<section>
+		<form action="GET" id="unit_form">
+			<label for="length">Select Length:</label><br>
+			<select name="length" form="unit_form" id="length">
+				<option value="1">1</option>
+				<option value="2">2</option>
+				<option value="3">3</option>
+				<option value="4">4</option>
+				<option value="5">5</option>
+				<option value="6">6</option>
+				<option value="7">7</option>
+				<option value="8">8</option>
+				<option value="9">9</option>
+				<option value="10">10</option>
+				<option value="11">11</option>
+				<option value="12">12</option>
+			</select>
+			<br><br>
+			<label for="width">Select Width:</label><br>
+			<select name="length" form="unit_form" id="width">
+				<option value="1">1</option>
+				<option value="2">2</option>
+				<option value="3">3</option>
+				<option value="4">4</option>
+				<option value="5">5</option>
+				<option value="6">6</option>
+				<option value="7">7</option>
+				<option value="8">8</option>
+				<option value="9">9</option>
+				<option value="10">10</option>
+				<option value="11">11</option>
+				<option value="12">12</option>
+			</select>
+		</form>
+	</section>
+
 	
 	
-<?php 
-	
-	include("../partials/footer.php");
-	include("model.php");?>	
-	<h4><?php print_r(error_get_last());?></h4>
+<?php include("../partials/footer.php");
+include("model.php");?>
 
--- pricing/model.php
+++ pricing/model.php
@@ -1,32 +1,30 @@
 <?php 
-echo "hello";
+
 	//put all of the business logic in this file. 
 	//Use PHP PDO to interface with DB
-//'sql5c2b.megasqlservers.com','bidwellsel413306','cikXF%24'
+	//'sql5c2b.megasqlservers.com','bidwellsel413306','cikXF%24'
+	//have try-catch block for each point of interaction, use when interacting with external object
 
-//have try-catch block for each point of interaction, use when interacting with external object
-try{
+//CONNECT TO DATABASE AND EXTRACT
+	try{
 
-	$db = new PDO("mysql:host=localhost;dbname=UNITS;port=8889","root","root");
-	$db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-	$db->exec("SET NAMES utf8");
-	var_dump($db);
+		$db = new PDO("mysql:host=localhost;dbname=UNITS;port=8889","root","root");
+		$db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+		$db->exec("SET NAMES utf8");
+		var_dump($db);
 
-}	catch(Exception $e){
-	echo "could not connect to db";
-	exit;
-}
-try{
-	$results = $db->query("SELECT sUnitName,iFloor,dcWidth,dcLength,dcStdRate,dcStdWeeklyRate,bPower,bClimate,bAlarm,bRentable,bRented FROM UNITS");
-	$data = $results->fetchAll();
-	echo "successfull query executed";
-}	catch(Exception $e){
-	echo "could not query the database";
-	exit;
-}
-echo "<pre>";
-echo $data[0][1];
-var_dump($data[0]);
-// var_dump($data);
+	}	catch(Exception $e){
+		echo "could not connect to db";
+		exit;
+	}
+	try{
+		$results = $db->query("SELECT sUnitName,iFloor,dcWidth,dcLength,dcStdRate,dcStdWeeklyRate,bPower,bClimate,bAlarm,bRentable,bRented FROM UNITS");
+		$data = $results->fetchAll();
+	}	catch(Exception $e){
+		echo "could not query the database";
+		exit;
+	}
+	echo "<pre>";
+	var_dump($data[0]);
 
-?>
+	?>
